8e99820f162cf3b0debadc70596f3cd4089dbd9d
RichardKobay <ricardo.roel110@gmail.com>
2024-02-02 19:28:42 -0600

    MOdifieed

--- src/main/java/edu/upvictoria/fpoo/Main.java
+++ src/main/java/edu/upvictoria/fpoo/Main.java
@@ -1,12 +1,13 @@
 package edu.upvictoria.fpoo;
 
-// import edu.upvictoria.fpoo.EstructurasSecuenciales.EjercicioUno;
-// import edu.upvictoria.fpoo.*;
+import edu.upvictoria.fpoo.estructuras.selectivas.EjercicioDiecinueve;
+import edu.upvictoria.fpoo.estructuras.selectivas.EjercicioUno;
+import edu.upvictoria.fpoo.helpers.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
-
-        // EjercicioUno uno = new EjercicioUno();
+    public static void main(String[] args){
+        Scanner scanner = new Scanner();
+        boolean x = Boolean.parseBoolean(scanner.readLine());
+        System.out.println(x);
     }
 }
---
+++ src/main/java/edu/upvictoria/fpoo/Menu.java
@@ -0,0 +1,11 @@
+package edu.upvictoria.fpoo;
+
+public class Menu {
+    void printMenu() {
+        int opc;
+        while (true) {
+            System.out.println("Select an option");
+            break;
+        }
+    }
+}
