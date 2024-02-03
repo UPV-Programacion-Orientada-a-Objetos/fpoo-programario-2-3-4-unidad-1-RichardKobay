afb794de335fe68579160b3dec34c92763261a1c
RichardKobay <ricardo.roel110@gmail.com>
2024-02-02 23:55:24 -0600

    menu in main

--- src/main/java/edu/upvictoria/fpoo/Main.java
+++ src/main/java/edu/upvictoria/fpoo/Main.java
@@ -6,8 +6,7 @@ import edu.upvictoria.fpoo.helpers.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner scanner = new Scanner();
-        boolean x = Boolean.parseBoolean(scanner.readLine());
-        System.out.println(x);
+        Menu menu = new Menu();
+        menu.printPackages();
     }
 }
